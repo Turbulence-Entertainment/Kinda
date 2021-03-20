edffb22257c96b2679d6e0b2064fb391c6eaa093
GuruShiksha <jyotish.nayak.official@gmail.com>
2021-03-20 15:26:27 +0530

    v1

---
+++ app/src/main/java/com/qboxus/musictok/ApiClasses/ApiLinks.java
@@ -0,0 +1,135 @@
+package com.qboxus.musictok.ApiClasses;
+
+public class ApiLinks {
+
+
+    public static final String BASE_URL ="https://kindawebtechnologies.com/mobileapp_api/";
+    public static final String API_BASE_URL =BASE_URL+"api/";
+     public static final String API_KEY="156c4675-9608-4591-1111-00000";
+
+    public static final String privacy_policy="https://www.privacypolicygenerator.info/live.php?token=";
+    public static final String terms_conditions="https://www.privacypolicygenerator.info/live.php?token=";
+
+
+
+    public static final String registerUser = API_BASE_URL +"registerUser";
+
+    public static final String login = API_BASE_URL +"login";
+
+    public static final String verifyPhoneNo = API_BASE_URL +"verifyPhoneNo";
+
+    public static final String addDeviceData = API_BASE_URL +"addDeviceData";
+
+    public static final String registerDevice= API_BASE_URL +"registerDevice";
+
+    public static final String showDeviceDetail= API_BASE_URL +"showDeviceDetail";
+
+    public static final String postVideo = API_BASE_URL +"postVideo";
+
+    public static final String showRelatedVideos = API_BASE_URL +"showRelatedVideos";
+
+    public static final String showFollowingVideos = API_BASE_URL +"showFollowingVideos";
+
+    public static final String showVideosAgainstUserID= API_BASE_URL +"showVideosAgainstUserID";
+
+    public static final String showUserDetail = API_BASE_URL +"showUserDetail";
+
+    public static final String likeVideo = API_BASE_URL +"likeVideo";
+
+    public static final String watchVideo = API_BASE_URL +"watchVideo";
+
+    public static final String showSounds = API_BASE_URL +"showSounds";
+
+    public static final String addSoundFavourite = API_BASE_URL +"addSoundFavourite";
+
+    public static final String showFavouriteSounds = API_BASE_URL +"showFavouriteSounds";
+
+    public static final String showUserLikedVideos = API_BASE_URL +"showUserLikedVideos";
+
+    public static final String followUser = API_BASE_URL +"followUser";
+
+    public static final String showDiscoverySections = API_BASE_URL +"showDiscoverySections";
+
+    public static final String showVideoComments= API_BASE_URL +"showVideoComments";
+
+    public static final String postCommentOnVideo = API_BASE_URL +"postCommentOnVideo";
+
+    public static final String postCommentReply = API_BASE_URL +"postCommentReply";
+
+
+    public static final String likeComment = API_BASE_URL +"likeComment";
+
+    public static final String likeCommentReply = API_BASE_URL +"likeCommentReply";
+
+
+    public static final String edit_profile= API_BASE_URL +"editProfile";
+
+    public static final String showFollowers = API_BASE_URL +"showFollowers";
+
+    public static final String showFollowing = API_BASE_URL +"showFollowing";
+
+
+    public static final String showVideosAgainstHashtag = API_BASE_URL +"showVideosAgainstHashtag";
+
+    public static final String sendPushNotification= API_BASE_URL +"sendMessageNotification";
+
+    public static final String addUserImage = API_BASE_URL +"addUserImage";
+
+    public static final String deleteVideo = API_BASE_URL +"deleteVideo";
+
+    public static final String search= API_BASE_URL +"search";
+
+    public static final String showVideoDetail= API_BASE_URL +"showVideoDetail";
+
+    public static final String showAllNotifications = API_BASE_URL +"showAllNotifications";
+
+    public static final String userVerificationRequest = API_BASE_URL +"userVerificationRequest";
+
+    public static final String downloadVideo = API_BASE_URL +"downloadVideo";
+
+    public static final String deleteWaterMarkVideo= API_BASE_URL +"deleteWaterMarkVideo";
+
+    public static final String showAppSlider= API_BASE_URL +"showAppSlider";
+
+    public static final String logout= API_BASE_URL +"logout";
+
+    public static final String showVideosAgainstSound= API_BASE_URL +"showVideosAgainstSound";
+
+    public static final String showReportReasons= API_BASE_URL +"showReportReasons";
+
+    public static final String reportVideo= API_BASE_URL +"reportVideo";
+
+    public static final String NotInterestedVideo= API_BASE_URL +"NotInterestedVideo";
+
+    public static final String addVideoFavourite= API_BASE_URL +"addVideoFavourite";
+
+    public static final String showFavouriteVideos= API_BASE_URL +"showFavouriteVideos";
+
+    public static final String updatePushNotificationSetting= API_BASE_URL +"updatePushNotificationSettings";
+
+
+    public static final String updateVideoDetail= API_BASE_URL +"updateVideoDetail";
+
+    public static final String addPolicySetting= API_BASE_URL +"addPrivacySetting";
+
+    public static final String addHashtagFavourite= API_BASE_URL +"addHashtagFavourite";
+
+    public static final String showFavouriteHashtags= API_BASE_URL +"showFavouriteHashtags";
+
+    public static final String reportUser= API_BASE_URL +"reportUser";
+
+
+    public static final String forgotPassword= API_BASE_URL +"forgotPassword";
+
+    public static final String verifyforgotPasswordCode= API_BASE_URL +"verifyforgotPasswordCode";
+
+    public static final String changePasswordForgot= API_BASE_URL +"changePasswordForgot";
+
+    public static final String changePassword= API_BASE_URL +"changePassword";
+
+    public static final String showSoundsAgainstSection= API_BASE_URL +"showSoundsAgainstSection";
+
+
+
+
+}
